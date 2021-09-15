9e6c87dc05f1f5ec2d0bde3c13db42372680b7ed
John Duimovich <$MY_GITHUB_EMAIL>
2021-09-15 09:01:59 -0400

    debug messages

--- all.sh
+++ all.sh
@@ -1,9 +1,19 @@
  
+export LD_LIBRARY_PATH=$(pwd)
 
-export LD_LIBRARY_PATH=$(pwd) 
-
+echo "2 runs of single linked app"
 ./main 
+./main 
+
+echo "2 runs of single linked app with nopie" 
 ./main_nopie
+./main_nopie
+
+
+echo "2 runs of app using shared lib"  
 ./main_so 
+./main_so 
+
+echo "2 runs of app using shared lib with nopie" 
 ./main_so_nopie
 
--- run.sh
+++ run.sh
@@ -17,10 +17,5 @@ cc  -c -no-pie slib2.c
 cc -shared -o libslib_nopie.so  slib1.o slib2.o
 cc -DMODE=\"shared-no-pie\"  -no-pie main.c -L $(pwd) -lslib_nopie -o  main_so_nopie
 
-export LD_LIBRARY_PATH=$(pwd)
-
-./main 
-./main_nopie
-./main_so 
-./main_so_nopie
+sh all.sh
 
