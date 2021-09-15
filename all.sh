6eab92472a9b3696383b32687851a98cfa03980a
John Duimovich <$MY_GITHUB_EMAIL>
2021-09-15 11:18:04 -0400

    x86_64

--- all.sh
+++ all.sh
@@ -6,28 +6,28 @@ echo "2 runs of single linked app"
 ./main 
 
 echo "2 runs of single linked app addr-no-randomize"
-setarch --addr-no-randomize ./main
-setarch --addr-no-randomize ./main
+setarch x86_64 --addr-no-randomize ./main
+setarch x86_64 --addr-no-randomize ./main
 
 echo "2 runs of single linked app with nopie --addr-no-randomize" 
 ./main_nopie
 ./main_nopie
 echo "2 runs of single linked app with nopie" 
-setarch --addr-no-randomize ./main_nopie
-setarch --addr-no-randomize ./main_nopie
+setarch x86_64 --addr-no-randomize ./main_nopie
+setarch x86_64 --addr-no-randomize ./main_nopie
 
 echo "2 runs of app using shared lib --addr-no-randomize "  
 ./main_so 
 ./main_so 
 echo "2 runs of app using shared lib"  
-setarch --addr-no-randomize  ./main_so 
-setarch --addr-no-randomize  ./main_so 
+setarch x86_64 --addr-no-randomize  ./main_so 
+setarch x86_64 --addr-no-randomize  ./main_so 
 
 echo "2 runs of app using shared lib with nopie" 
 ./main_so_nopie
 ./main_so_nopie
 
 echo "2 runs of app using shared lib with nopie --addr-no-randomize " 
-setarch --addr-no-randomize ./main_so_nopie
-setarch --addr-no-randomize ./main_so_nopie
+setarch x86_64 --addr-no-randomize ./main_so_nopie
+setarch x86_64 --addr-no-randomize ./main_so_nopie
 
