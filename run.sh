138ce63bd94d9529d526938e589583863a6b2356
John Duimovich <$MY_GITHUB_EMAIL>
2021-09-15 08:23:12 -0400

    simpler scripts

--- run.sh
+++ run.sh
@@ -1,7 +1,26 @@
- 
+
+cc  -c slib1.c
+cc  -c slib2.c
+cc  main.c slib1.o slib2.o -o  main 
+
 cc  -c -no-pie slib1.c
 cc  -c -no-pie slib2.c
-cc -no-pie main.c slib1.o slib2.o -o  main_no_pie
+cc -no-pie main.c slib1.o slib2.o -o  main_nopie
 
-./main_no_pie
+cc  -c slib1.c
+cc  -c  slib2.c
+cc -shared -o libslib.so  slib1.o slib2.o
+cc  main.c -L $(pwd) -lslib -o  main_so 
+
+cc  -c -no-pie slib1.c
+cc  -c -no-pie slib2.c
+cc -shared -o libslib_nopie.so  slib1.o slib2.o
+cc -no-pie main.c -L $(pwd) -lslib_nopie -o  main_so_nopie
+
+export LD_LIBRARY_PATH=$(pwd)
+
+./main 
+./main_nopie
+./main_so 
+./main_so_nopie
 
--- runit.sh
+++ runit.sh
@@ -1,5 +1,9 @@
-/app/main_no_pie
+ 
 
-export LD_LIBRARY_PATH=$(pwd)
-/app/main_so_nopie
+export LD_LIBRARY_PATH=$(pwd) 
+
+./main 
+./main_nopie
+./main_so 
+./main_so_nopie
 
