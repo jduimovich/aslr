939c4de96c95634dda57165b5daae3ccc7ad2b08
John Duimovich <$MY_GITHUB_EMAIL>
2021-09-15 08:51:01 -0400

    error in define

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
 
 cc  -c slib1.c
 cc  -c slib2.c
-cc -DMODE=linked  main.c slib1.o slib2.o -o  main 
+cc -DMODE="linked"  main.c slib1.o slib2.o -o  main 
 
 cc  -c -no-pie slib1.c
 cc  -c -no-pie slib2.c
-cc -DMODE=linked-no-pie -no-pie main.c slib1.o slib2.o -o  main_nopie
+cc -DMODE="linked-no-pie" -no-pie main.c slib1.o slib2.o -o  main_nopie
 
 cc  -c slib1.c
 cc  -c  slib2.c
 cc -shared -o libslib.so  slib1.o slib2.o
-cc -DMODE=shared  main.c -L $(pwd) -lslib -o  main_so 
+cc -DMODE="shared"  main.c -L $(pwd) -lslib -o  main_so 
 
 cc  -c -no-pie slib1.c
 cc  -c -no-pie slib2.c
 cc -shared -o libslib_nopie.so  slib1.o slib2.o
-cc -DMODE=shared-no-pie  -no-pie main.c -L $(pwd) -lslib_nopie -o  main_so_nopie
+cc -DMODE="shared-no-pie"  -no-pie main.c -L $(pwd) -lslib_nopie -o  main_so_nopie
 
 export LD_LIBRARY_PATH=$(pwd)
 
